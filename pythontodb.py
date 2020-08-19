eea4c743b56e348b0535ae8ab12ad90a409bade4
lakshmanmunaga <lakshmanm0101@gmail.com>
2020-08-19 21:24:15 +0530

    Update pythontodb.py

--- pythontodb.py
+++ pythontodb.py
@@ -1,6 +1,6 @@
 import mysql.connector
 import csv
-
+# create database
 mydb = mysql.connector.connect(
     host='localhost',
     username='root',
@@ -8,11 +8,17 @@ mydb = mysql.connector.connect(
     database='metadata'
 )
 print("database connected successfully")
+
 mycurser = mydb.cursor()
+# read file using csv 
 file1 = csv.reader(open('test.csv'))
+# insert data into sql
 sql = 'INSERT INTO user(name,email,password,age) VALUES(%s,%s,%s,%s)'
 for row in file1:
     mycurser.execute(sql, row)
     print(row)
 mydb.commit()
+#close the curser
 mycurser.close()
+#close the database
+mydb.close()
